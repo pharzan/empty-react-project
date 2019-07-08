13b63d14d33a6e52da7b13797da66f1e5b7e82eb
Farzan <farzan.tinati@gmail.com>
2019-07-08 12:40:52 +0300

    added project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dummy_Component from "dummy-component";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Dummy_Component/>
       </header>
     </div>
   );
